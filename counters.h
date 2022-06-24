f661dbe70780d7623331e8c2c43a90415a96648c
Aleksey Mandrykin <almandrykin@ya.ru>
2022-06-24 20:00:28 +0300

    Counters

---
+++ counters.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+namespace xameleon 
+{
+    typedef struct session_counters {
+        int                 total_request;
+        int                 bad_request;
+        int                 not_found;
+        int                 http2https;
+        session_counters() : total_request(0), bad_request(0), not_found(0), http2https(0) {}
+    } http_session_counters_t;
+}
